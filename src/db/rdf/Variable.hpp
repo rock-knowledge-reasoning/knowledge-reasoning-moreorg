c6915afa72bbdee93824b4e121b3a676b9c92ab8
Thomas Roehr <thomas.roehr@dfki.de>
2014-07-05 19:31:32 +0200

    Add documentation to Variable

--- src/db/rdf/Variable.hpp
+++ src/db/rdf/Variable.hpp
@@ -8,14 +8,40 @@ namespace owl_om {
 namespace db {
 namespace query {
 
+/**
+ * Variable represent a query object. It can be either an
+ * ungrounded variable such as '?s' or a prefix or fully qualified IRI
+ */
 class Variable : public owlapi::model::IRI
 {
 public:
+    /**
+     * Default Variable constructor
+     */
     Variable();
+
+    /**
+     * Constructor to create variable from IRI
+     */
     Variable(const IRI& iri);
+
+    /**
+     * Constructor to create ground or ungrounded variables
+     */
     Variable(const std::string& name, bool grounded);
 
+    /**
+     * Get name of this variable, i.e. for an ungrounded variable
+     * it removes the leading question mark
+     */
     std::string getName() const;
+
+    /**
+     * Get query name of this variable, ungrounded variables
+     * will be presented with leading question marks, 
+     * and URI will be quoted, e.g., <http://www.w3.org/>
+     * \return Query name of this variable
+     */
     std::string getQueryName() const;
 
     /**
