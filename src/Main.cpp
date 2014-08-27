651c8369ef911fb152eb1a19bef65a506747c176
Thomas Roehr <thomas.roehr@dfki.de>
2014-08-28 01:10:10 +0200

    Adapt main to testing script

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <stdio.h>
-#include <owl_om/Ontology.hpp>
+#include <owl_om/OrganizationModel.hpp>
 
 int main(int argc, char** argv)
 {
@@ -15,12 +15,23 @@ int main(int argc, char** argv)
 
     if(!o_filename.empty())
     {
-        Ontology::Ptr ontology = Ontology::fromFile( o_filename );
-        db::query::Results results = ontology->findAll(db::query::Subject(), db::query::Predicate(), db::query::Object());
-        printf("Results:\n%s", results.toString().c_str());
-        printf("\n-----------------------------------\n");
-        printf("Resulting Ontology:\n%s", ontology->toString().c_str());
+        OrganizationModel om( o_filename );
+
+        using namespace owl_om;
+        using namespace owl_om::vocabulary;
+
+        om.createNewFromModel(OM::resolve("Sherpa"), true);
+        om.createNewFromModel(OM::resolve("CREX"), true);
+        om.createNewFromModel(OM::resolve("CREX"), true);
+        om.createNewFromModel(OM::resolve("PayloadCamera"), true);
+        om.createNewFromModel(OM::resolve("PayloadCamera"), true);
+
+        om.refresh();
+
+        printf("Resulting Ontology:\n%s", om.ontology()->toString().c_str());
+        printf("%s", om.getStatistics().toString().c_str());
     } else {
         fprintf(stderr, "usage: %s <filename>\n", argv[0]);
     }
+
 }
