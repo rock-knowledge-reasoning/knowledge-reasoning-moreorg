e6f9afef31f27cee71920e248710f42febb07331
Thomas Roehr <thomas.roehr@dfki.de>
2014-09-02 20:14:10 +0200

    Add scenario setup to main

--- src/Main.cpp
+++ src/Main.cpp
@@ -23,9 +23,9 @@ int main(int argc, char** argv)
         using namespace owl_om::vocabulary;
 
         om.createNewFromModel(OM::resolve("Sherpa"), true);
-//        om.createNewFromModel(OM::resolve("Sherpa"), true);
-//        om.createNewFromModel(OM::resolve("Sherpa"), true);
-//        om.createNewFromModel(OM::resolve("Sherpa"), true);
+        om.createNewFromModel(OM::resolve("BaseCamp"), true);
+        om.createNewFromModel(OM::resolve("BaseCamp"), true);
+        om.createNewFromModel(OM::resolve("BaseCamp"), true);
         om.createNewFromModel(OM::resolve("CREX"), true);
         om.createNewFromModel(OM::resolve("CREX"), true);
         om.createNewFromModel(OM::resolve("PayloadCamera"), true);
@@ -38,28 +38,9 @@ int main(int argc, char** argv)
         om.createNewFromModel(OM::resolve("PayloadCamera"), true);
         om.createNewFromModel(OM::resolve("PayloadCamera"), true);
         om.createNewFromModel(OM::resolve("PayloadCamera"), true);
-//        om.createNewFromModel(OM::resolve("PayloadCamera"), true);
 
-//        om.refresh();
-//        InterfaceCombinationList d = om.generateInterfaceCombinationsCCF();
-        InterfaceCombinationList d = om.generateInterfaceCombinations();
-
-        //BOOST_FOREACH(const std::vector<InterfaceConnection>& composite, d)
-        //{
-        //    printf("Composite:\n");
-        //    BOOST_FOREACH(const InterfaceConnection& ic, composite)
-        //    {
-        //        printf("    %s\n", ic.toString().c_str());
-        //    }
-        //}
-        //printf("Resulting combinations #: %d\n", d.size());
-        //Statistics stats = om.getStatistics();
-        //printf("Stats: %s\n", stats.toString().c_str());
-        //exit(0);
-
-
-        printf("Resulting Ontology:\n%s", om.ontology()->toString().c_str());
-        printf("%s", om.getStatistics().toString().c_str());
+        om.refresh(true);
+        std::cout << om.getStatistics();
     } else {
         fprintf(stderr, "usage: %s <filename>\n", argv[0]);
     }
