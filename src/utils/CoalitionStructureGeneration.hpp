16b45de5c8a74da6d524998f85bb4ac13cbd0b4b
Thomas Roehr <thomas.roehr@dfki.de>
2014-10-30 14:41:47 +0100

    ccf: Add debug and fixes to subspace search

--- src/utils/CoalitionStructureGeneration.hpp
+++ src/utils/CoalitionStructureGeneration.hpp
@@ -117,7 +117,7 @@ public:
     }
 
     /**
-     * Compute the integer partitions and the agent coalition map for coalition size up to 
+     * Compute the integer partitions and the agent coalition map for coalition size up to
      * the maximum number of agents
      */
     void prepare()
@@ -236,7 +236,7 @@ public:
 
     Bounds computeIntegerPartitionBounds(const IntegerPartition& partition)
     {
-        IntegerPartition::const_iterator cit = partition.begin(); 
+        IntegerPartition::const_iterator cit = partition.begin();
         Bounds bounds;
         bounds.maximum = 0;
         bounds.average = 0;
@@ -315,8 +315,9 @@ public:
                 break;
             }
 
+            double quality = 1.0;
             LOG_DEBUG_S << "Compute best coalition structure for this subspace: " << partition;
-            CoalitionStructure coalitionStructure = searchSubspace(partition, 0, 0, mAgents, bestCoalitionStructure, bestCoalitionStructureValue, CoalitionStructure(), 0, globalUpperBound);
+            CoalitionStructure coalitionStructure = searchSubspace(partition, 0, 0, mAgents, bestCoalitionStructure, bestCoalitionStructureValue, CoalitionStructure(), globalUpperBound, quality);
 
             // No improvement of the results
             if(coalitionStructure == bestCoalitionStructure)
@@ -332,7 +333,7 @@ public:
 
                 LOG_DEBUG_S << "Best coalition found so far: " << bestCoalitionStructure << ", " << bounds;
 
-                // TODO: pick the next one for evaluation and repeat until satified with 
+                // TODO: pick the next one for evaluation and repeat until satified with
                 // the solution
                 //
                 boundMap = prune(boundMap);
@@ -347,7 +348,7 @@ public:
         }
 
         LOG_DEBUG_S << "Best coalition found: " << bestCoalitionStructure << ", " << mCoalitionStructureValueFunction(bestCoalitionStructure);
-         
+
         //// search next one
         //if(boundMap.size() == 1)
         //{
@@ -383,7 +384,7 @@ public:
         // consider betaStar > 1, where betaStar = 1.05 "means that the solution sought
         // needs to hava a value that is at least 95% of the optimal one"
         //
-        // select to search the smallest sub-space that could, potentially, give a value greater than 
+        // select to search the smallest sub-space that could, potentially, give a value greater than
         // or equal to (globalUpperBound/betaStar)
         //
         //      Select G (the IntegerPartition) = argumin_{G \in \mathcal{G'}: UpperBound_G \gt \frac{globalUpperBound}{{betaStar} (|P_{g}|)
@@ -397,12 +398,14 @@ public:
      * \param globalUpperBound
      * \param bestStar Quality of the solution, i.e. 1.05 means 95% percent of the optimal solution
      */
-    CoalitionStructure searchSubspace(const IntegerPartition& partition, size_t k, size_t alpha, const AgentList& agents, CoalitionStructure bestStructure, double bestStructureValue, const CoalitionStructure& currentStructure, double globalUpperBound = std::numeric_limits<double>::max(), double betaStar = 1.0)
+    CoalitionStructure searchSubspace(const IntegerPartition& partition, size_t k, size_t alpha, const AgentList& agents, CoalitionStructure bestStructure, double bestStructureValue, const CoalitionStructure& currentStructure, double globalUpperBound, double betaStar)
     {
-        LOG_DEBUG_S << "Search subspace of current structure: " << currentStructure;
+        std::string indent(4*k, ' ');
+        LOG_DEBUG_S << indent << " search subspace of current structure: " << currentStructure;
+
         if(k > 0 && partition[k] != partition[k-1])
         {
-            LOG_DEBUG_S << "Search subspace for " << k << "partition[k]: " << partition[k] << " != " << partition[k-1];
+            LOG_DEBUG_S << indent << " search subspace for k: " << k << ", partition[k]: " << partition[k] << " != " << partition[k-1];
             // resetting alpha when the size is not repeated
             alpha = 1;
         }
@@ -413,7 +416,7 @@ public:
         {
             indexList.push_back(i);
         }
-        LOG_DEBUG_S << "Indexlist initialized: " << indexList << " agents: " << agents.size();
+        LOG_DEBUG_S << indent << " indexlist initialized: " << indexList << " agents: " << agents.size();
 
         // Compute upper bound for M_{k,0} to avaoid redundant computations
         size_t upperBoundM_k = mAgents.size() + 1;
@@ -421,21 +424,22 @@ public:
         {
             upperBoundM_k += partition[i]*IntegerPartitioning::multiplicity(partition, partition[i]);
         }
-        LOG_DEBUG_S << "UpperBound M_k: " << upperBoundM_k;
+        LOG_DEBUG_S << indent << " upperBound M_k: " << upperBoundM_k;
 
         // Compute upper bound of subspace, so that we can stop computation when this maximum has
         // been found
         double upperBoundOfSubspace = mIntegerPartitionBoundsMap[partition].maximum;
-        LOG_DEBUG_S << "UpperBound of subspace " << partition << ": " << upperBoundOfSubspace;
+        LOG_DEBUG_S << indent << " upperBound of subspace " << partition << ": " << upperBoundOfSubspace;
 
         using namespace base::combinatorics;
         Combination<int> combinations(indexList, partition[k], EXACT);
         do {
             std::vector<int> m_k = combinations.current();
-            LOG_DEBUG_S << "Current combination m_k=" << m_k << ", alpha=" << alpha;
-            if(alpha <= m_k[0] && m_k[0] <= upperBoundM_k)
+            LOG_DEBUG_S << indent << " current combination m_k=" << m_k << ", alpha=" << alpha;
+            // m_k[0] + 1: we start with index 0
+            if(alpha <= m_k[0] + 1 && m_k[0] + 1 <= upperBoundM_k)
             {
-                LOG_DEBUG_S << "Computing combination";
+                LOG_DEBUG_S << indent << " computing combination";
                 // Map combination of indices to current coalition, i.e. an agent (sub)set
                 AgentList remainingAgents = agents;
                 std::vector<Agent> coalition;
@@ -449,24 +453,25 @@ public:
                 CoalitionStructure coalitionStructure = currentStructure;
                 coalitionStructure.push_back(coalition);
 
-                LOG_DEBUG_S << "Partial coalition structure: " << coalitionStructure << " , remaining agents " << remainingAgents;
+                LOG_DEBUG_S << indent << " current coalition structure: " << currentStructure;
+                LOG_DEBUG_S << indent << " partial coalition structure: " << coalitionStructure << " , remaining agents " << remainingAgents;
 
                 double currentStructureValue = mCoalitionStructureValueFunction( coalitionStructure );
 
                 // Check if we reached the end
                 if(k == partition.size() - 1)
                 {
-                    LOG_DEBUG_S << "Reached the end at k: " << k << " -- bestValue: " << bestStructureValue << " vs. " << currentStructureValue;
-                    if(currentStructureValue > bestStructureValue) 
+                    LOG_DEBUG_S << indent << " reached the end at k: " << k << " -- bestValue: " << bestStructureValue << " vs. " << currentStructureValue;
+                    if(currentStructureValue > bestStructureValue)
                     {
                         // update the currently best coalition structure
                         bestStructure = coalitionStructure;
                         bestStructureValue = currentStructureValue;
 
-                        LOG_DEBUG_S << "Update current best: " << bestStructure << ", value " << bestStructureValue;
+                        LOG_DEBUG_S << indent << " update current best: " << bestStructure << ", value " << bestStructureValue;
                     }
                 } else {
-                    LOG_DEBUG_S << "Computing subspace potential";
+                    LOG_DEBUG_S << indent << " computing subspace potential";
                     // Estimate the potential of this subspace using
                     // know value for the already found coalitions
                     // and existing upper bounds for the coalitions still
@@ -477,7 +482,7 @@ public:
                     for( size_t i = 0; i < coalitionStructure.size(); ++i)
                     {
                         double valueOfCoalition = mCoalitionValueFunction( coalitionStructure[i] );
-                        LOG_DEBUG_S << "Coalition: " << coalitionStructure[i] << " value: " << valueOfCoalition;
+                        LOG_DEBUG_S << indent << " coalition: " << coalitionStructure[i] << " value: " << valueOfCoalition;
                         subspacePotentialValue += valueOfCoalition;
                     }
 
@@ -487,37 +492,53 @@ public:
                     for( size_t i = coalitionStructure.size(); i < partition.size(); ++i)
                     {
                         double max_s = mCoalitionBoundMap[ partition[i] ].maximum;
-                        LOG_DEBUG_S << "Potential for coalition size: " << partition[i] << ": " << max_s;
+                        LOG_DEBUG_S << indent << " potential for coalition size: " << partition[i] << ": " << max_s;
                         subspacePotentialValue += max_s;
                     }
-                    LOG_DEBUG_S << "Subspace potential: " << subspacePotentialValue;
+                    LOG_DEBUG_S << indent << " subspace potential: " << subspacePotentialValue;
 
                     // Check if it is worth to enter this subspace
                     if(bestStructureValue < subspacePotentialValue)
                     {
-                        LOG_DEBUG_S << "Continue search: best: " << bestStructureValue << " vs. subspace potential: " <<  subspacePotentialValue;
+                        LOG_DEBUG_S << indent << " continue search: best: " << bestStructureValue << " vs. subspace potential: " <<  subspacePotentialValue;
                         bestStructure = searchSubspace(partition, k+1, m_k[0], remainingAgents, bestStructure, bestStructureValue, coalitionStructure, globalUpperBound, betaStar);
+                        bestStructureValue = mCoalitionStructureValueFunction( bestStructure );
                     } else {
-                        LOG_DEBUG_S << "Pruning: insufficient subspace potential: best: " << bestStructureValue << " vs. " <<  subspacePotentialValue;
+                        LOG_DEBUG_S << indent << " pruning: insufficient subspace potential: best: " << bestStructureValue << " vs. " <<  subspacePotentialValue;
                     }
                 }
 
-                // Stop if the required solution has been found or if the current best 
+                // Stop if the required solution has been found or if the current best
                 // is equal to the upper bound of this sub-space
-                if( globalUpperBound / bestStructureValue <= betaStar || bestStructureValue == upperBoundOfSubspace )
+                if( globalUpperBound / bestStructureValue <= betaStar )
                 {
+
+                    LOG_DEBUG_S << indent << " prematurely returning best structure: " << bestStructure;
+                    LOG_DEBUG_S << "Quality sufficient: " << globalUpperBound/bestStructureValue << "<=" << betaStar;
                     return bestStructure;
                 }
+
+                if(bestStructureValue == upperBoundOfSubspace)
+                {
+                    LOG_DEBUG_S << indent << " prematurely returning best structure: " << bestStructure;
+                    LOG_DEBUG_S << indent << " bestStructureValue == upperBoundOfSubspace, both are " << bestStructureValue;
+                    return bestStructure;
+                }
+            } else {
+            // if(alpha <= m_k[0] && m_k[0] <= upperBoundM_k)
+                LOG_DEBUG_S << indent << " skipping: condition does not hold: alpha < m_k[0]+1 && m_k[0]+1 <= upperBoundM_k"
+                    << ", alpha= " << alpha << ", m_k[0]=" << m_k[0] << ", upperBoundM_k=" << upperBoundM_k;
             }
         } while(combinations.next());
 
+        LOG_DEBUG_S << indent << " returning best structure: " << bestStructure;
         return bestStructure;
     }
 
     std::string toString()
     {
         std::stringstream ss;
-        ss << "Agents: "; 
+        ss << "Agents: ";
         ss << "[";
         for(size_t i = 0; i < mAgents.size(); ++i)
         {
