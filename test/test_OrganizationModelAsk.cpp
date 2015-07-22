0a80a28e12316ad9e1f85af674e2628fec1f1684
Thomas Roehr <thomas.roehr@dfki.de>
2015-07-22 17:14:03 +0200

    Add tests for recursive resolution of service support. Actual fix is in owlapi.

--- test/test_OrganizationModelAsk.cpp
+++ test/test_OrganizationModelAsk.cpp
@@ -2,11 +2,46 @@
 #include <organization_model/OrganizationModel.hpp>
 #include <organization_model/OrganizationModelAsk.hpp>
 
+#include <owlapi/csp/ResourceMatch.hpp>
 #include "test_utils.hpp"
 
 using namespace organization_model;
 
 BOOST_AUTO_TEST_SUITE(organization_model_ask)
+
+BOOST_AUTO_TEST_CASE(recursive_resolution)
+{
+    using namespace owlapi::vocabulary;
+    using namespace owlapi::model;
+
+    OrganizationModel::Ptr om(new OrganizationModel(getRootDir() + "/test/data/om-schema-v0.8.owl"));
+    IRI sherpa = OM::resolve("Sherpa");
+    IRI locationImageProvider = OM::resolve("LocationImageProvider");
+
+    OrganizationModelAsk ask(om);
+    Service service(locationImageProvider);
+
+    IRIList combination;
+    combination.push_back(sherpa);
+
+    IRIList services;
+    services.push_back(service.getModel());
+
+    IRIList supportedServiceModels = owlapi::csp::ResourceMatch::filterSupportedModels(combination, services, om->ontology());
+
+    OWLOntologyAsk oAsk(om->ontology());
+    std::vector<OWLCardinalityRestriction::Ptr> restrictions = oAsk.getCardinalityRestrictions(locationImageProvider);
+    BOOST_TEST_MESSAGE("Cardinality: " << OWLCardinalityRestriction::toString(restrictions));
+    BOOST_REQUIRE_MESSAGE(!supportedServiceModels.empty(), "Supported serviced models: " << services << " models where: " << supportedServiceModels);
+
+    {
+        uint32_t saturationPoint = ask.getFunctionalSaturationBound(service, sherpa);
+        BOOST_REQUIRE_MESSAGE(saturationPoint == 1, "1 Sherpa sufficient for LocationImageProvider: was " << saturationPoint);
+
+        algebra::SupportType supportType = ask.getSupportType(service, sherpa, saturationPoint);
+        BOOST_REQUIRE_MESSAGE(supportType == algebra::FULL_SUPPORT, "Full support from sherpa for LocationImageProvider at saturation point");
+    }
+}
 
 BOOST_AUTO_TEST_CASE(functional_saturation)
 {
@@ -21,6 +56,7 @@ BOOST_AUTO_TEST_CASE(functional_saturation)
     IRI payloadCamera = OM::resolve("PayloadCamera");
 
     IRI stereoImageProvider = OM::resolve("StereoImageProvider");
+    IRI locationImageProvider = OM::resolve("LocationImageProvider");
 
     OrganizationModelAsk ask(om);
     {
@@ -45,6 +81,16 @@ BOOST_AUTO_TEST_CASE(functional_saturation)
 
             algebra::SupportType supportType = ask.getSupportType(service, payloadCamera, saturationPoint);
             BOOST_REQUIRE_MESSAGE(supportType == algebra::FULL_SUPPORT, "Full support from payload camera for StereoImageProvider at saturation point");
+        }
+    }
+    {
+        Service service(locationImageProvider);
+        {
+            uint32_t saturationPoint = ask.getFunctionalSaturationBound(service, sherpa);
+            BOOST_REQUIRE_MESSAGE(saturationPoint == 1, "1 Sherpa sufficient for StereoImageProvider: was " << saturationPoint);
+
+            algebra::SupportType supportType = ask.getSupportType(service, sherpa, saturationPoint);
+            BOOST_REQUIRE_MESSAGE(supportType == algebra::FULL_SUPPORT, "Full support from sherpa for StereoImageProvider at saturation point");
         }
     }
 
