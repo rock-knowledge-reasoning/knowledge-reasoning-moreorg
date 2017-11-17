bd329dad4c7e4cf1af7da3c43bb8c0c8427a5f4c
Thomas Roehr <thomas.roehr@dfki.de>
2017-11-17 16:48:59 +0100

    Add testing models

--- test/test_Lego.cpp
+++ test/test_Lego.cpp
@@ -66,5 +66,42 @@ BOOST_AUTO_TEST_CASE(list)
     }
 }
 
+BOOST_AUTO_TEST_CASE(icaps)
+{
+    OrganizationModel::Ptr om(new OrganizationModel(getRootDir() + "/test/data/om-icaps-v0.1.owl"));
+    OrganizationModelAsk ask(om);
+
+    owlapi::vocabulary::Custom lego("http://www.rock-robotics.org/2017/10/om-icaps#");
+
+    ModelPool modelPool;
+    modelPool[ lego.resolve("YellowAgent") ] = 4;
+    modelPool[ lego.resolve("GreenAgent") ] = 3;
+    modelPool[ lego.resolve("RedAgent") ] = 2;
+    modelPool[ lego.resolve("BlueAgent") ] = 1;
+    modelPool[ lego.resolve("Adapter") ] = 1;
+    modelPool[ lego.resolve("Manipulator") ] = 1;
+
+
+    size_t epochs = 1;
+    size_t minFeasible = 1;
+    for(size_t i = 0; i < epochs; ++i)
+    {
+        BaseGraph::Ptr baseGraph;
+        bool feasible = algebra::Connectivity::isFeasible(modelPool, ask, baseGraph, 0, minFeasible, organization_model::vocabulary::OM::resolve("MechanicalInterface") );
+        assert(feasible);
+        if(baseGraph)
+        {
+            std::stringstream ss;
+            ss << "/tmp/organization-model-icaps-";
+            ss << i;
+            ss << ".dot";
+
+            graph_analysis::io::GraphIO::write(ss.str(), baseGraph);
+        }
+        BOOST_TEST_MESSAGE(algebra::Connectivity::getStatistics().toString());
+        usleep(200);
+    }
+}
+
 BOOST_AUTO_TEST_SUITE_END()
 
