8b6a1ffaa53111c38a511ce40c30344973d667a9
Thomas Roehr <thomas.roehr@dfki.de>
2018-05-25 09:21:32 +0200

    Update AtomicAgent

--- src/AtomicAgent.hpp
+++ src/AtomicAgent.hpp
@@ -20,7 +20,8 @@ class AtomicAgent
     friend class boost::serialization::access;
     /// type of model this role fulfills
     owlapi::model::IRI mModel;
-    /// id or the role
+    /// Id of the role, i.e. unique for all agent of the
+    /// same model
     size_t mId;
     /// Name of the instance derived from model IRI and id
     std::string mName;
@@ -37,7 +38,8 @@ public:
     /**
      * Preferred role constructor
      * \param name Name/Id of the role
-     * \param model Model identification
+     * \param model Model identification id, which needs to be unique for all atomic
+     * agents of the same model
      */
     AtomicAgent(size_t id, const owlapi::model::IRI& model);
 
@@ -51,13 +53,31 @@ public:
     static std::string toString(const Set& agents, size_t indent = 0);
     static organization_model::ModelPool getModelPool(const List& agents);
     static organization_model::ModelPool getModelPool(const Set& agents);
+
+    /**
+     * Get a list of atomic agents from a model pool
+     * alias for createAtomicAgents
+     * \return agent list
+     */
+    static List fromModelPool(const ModelPool& modelPool) { createAtomicAgents(modelPool); }
     /**
      * Compute the cardinality of an agent model inside a given list of agents
+     * \return cardinality
      */
     static size_t getCardinality(const List& agents, const owlapi::model::IRI& agentModel) {
     return getModelPool(agents)[agentModel]; }
 
+    /**
+     * Get the intersection of two atomic agent sets
+     * \return The set of agent which are shared between two sets of atomic
+     * agents
+     */
     static AtomicAgent::List getIntersection(const AtomicAgent::Set& a0, const AtomicAgent::Set& a1);
+
+    /**
+     * Get the difference set between to atomic agent sets
+     * \return different set
+     */
     static AtomicAgent::List getDifference(const AtomicAgent::Set& a0, const AtomicAgent::Set& a1);
 
     bool operator<(const AtomicAgent& other) const;
@@ -80,13 +100,25 @@ public:
 
     /**
      * Create the robot facade
+     * \param organizationAsk oject including ontology to identify type
+     * information
+     * \return robot facade for this AtomicAgent
      */
     facades::Robot getFacade(const OrganizationModelAsk& ask) const;
 
     /**
-     * Compute the type map from a set of atomic agents
+     * Compute the type partitioned map for a set of atomic agents
+     * \param atomicAgents set of atomic agents for which a typemap shall be
+     * created
+     * \return TypeMap for given atomic agent set
      */
     static TypeMap toTypeMap(const AtomicAgent::Set& atomicAgents);
+
+    /**
+     * Stringify the type map
+     * \param typeMap typeMap to stringify
+     * \param indent indentation Number of spaces for indent
+     */
     static std::string toString(const TypeMap& typeMap, size_t indent = 0);
 };
 
